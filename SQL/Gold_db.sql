f29298e2147146fc7b0809655772d646c2520eb1
Aarif09 <105052361+Aarif09@users.noreply.github.com>
2025-04-05 18:03:25 +0530

    Add files via upload

---
+++ SQL/Gold_db.sql
@@ -0,0 +1,27 @@
+
+use gold_dbase;
+
+CREATE TABLE gold_dbase.transportation_gold AS 
+SELECT 
+    route_name,
+    COUNT(*) AS total_deliveries,
+    AVG(delivery_time) AS avg_delivery_time,
+    AVG(fuel_consumed) AS avg_fuel_consumed,
+    vehicle_id,
+    SUM(distance_covered) AS total_distance,
+    (SUM(distance_covered) / SUM(fuel_consumed)) AS fuel_efficiency,
+    driver_name,
+    delivery_status,
+    COUNT(DISTINCT delivery_id) AS total_deliveries_by_driver,
+    AVG(rating) AS driver_rating,
+    CURDATE() AS report_date
+FROM silver_db.delivery_data_silver
+GROUP BY route_name, vehicle_id, driver_name, delivery_status;
+
+select*from  transportation_gold;
+
+drop table transportation_gold;
+
+select *from silver_db.delivery_data_silver;
+
+desc gold_dbase.transportation_gold;
---
+++ SQL/Silver_db.sql
@@ -0,0 +1,5 @@
+create database silver_db;
+
+use silver_db;
+
+select * from delivery_data_silver;
